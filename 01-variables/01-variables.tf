099aaeab2cf1b34c1faed55031c7af084551e8f5
Afzal Basha <afzalbasha992@gmail.com>
2023-05-01 12:18:00 +0530

    changes

---
+++ 01-variables/01-variables.tf
@@ -0,0 +1,14 @@
+
+
+variable "sample_string"{
+  default="Hello world"
+}
+
+variable "sample_list"{
+  default=[
+  100,
+  "Hello",
+    12.5,
+    true
+  ]
+}
--- 01-variables/output.tf
+++ 01-variables/output.tf
@@ -1,9 +1,8 @@
 
-
-variable "sample_string"{
-  default="Hello world"
-}
-
 output "sample_string"{
   value="value of sample string = ${var.sample_string}"
+}
+
+output "sample_list2"{
+  value=var.sample_list[1]
 }
